612489a924539979b3df30d09de1d0bada9f7201
alireza jelokhani <alirezajelokhani559@yahoo.com>
2022-07-05 14:42:25 +0430

    Add consumer util

--- evaluator/src/main/java/utils/ConsumerUtil.java
+++ evaluator/src/main/java/utils/ConsumerUtil.java
@@ -1,4 +1,14 @@
 package utils;
 
+import model.CandleData;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Properties;
+
 public class ConsumerUtil {
+
+    public static KafkaConsumer<Long, CandleData> CreateConsumer(Properties props) {
+        return new KafkaConsumer<Long, CandleData>(props);
+    }
 }
