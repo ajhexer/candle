1cc06447f7d8ad5a26afc0d929a247e1c19b9127
alireza jelokhani <alirezajelokhani559@yahoo.com>
2022-07-06 23:16:28 +0430

    Add hibernate util class

---
+++ evaluator/src/main/java/utils/HibernateUtil.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sessionFactory;
+
+    public static synchronized SessionFactory getSessionFactory() {
+        if (sessionFactory != null) {
+            return sessionFactory;
+        }
+        sessionFactory = new Configuration().configure().buildSessionFactory();
+        return sessionFactory;
+    }
+
+
+}
